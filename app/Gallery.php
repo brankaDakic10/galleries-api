57fc5022f4001ad9500d58d474a9ee97db506675
brankaDakic10 <branka.dakic@yahoo.com>
2018-06-11 21:28:04 +0200

    Added tables galleries,images,users and comments

---
+++ app/Gallery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Gallery extends Model
+{
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function images() {
+        return $this->hasMany(Image::class);
+    }
+    
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
+}
--- database/migrations/2018_06_11_191842_create_comments_table.php
+++ database/migrations/2018_06_11_191842_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('firstName');
-            $table->string('lastName');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('content');
+            $table->unsignedInteger('gallery_id');
+
+
+            $table->foreign('gallery_id')
+            ->references('id')
+            ->on('galleries')
+            ->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -31,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 }
